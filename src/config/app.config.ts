9eb3529a3fe3b305767d7644d2e003cfbcabb1c1
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-11-01 11:52:03 +0400

    chore: add port

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,6 +4,7 @@ import * as Joi from 'joi';
 export default registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV,
   WalletPrivateKey: process.env.WALLET_PRIVATE_KEY,
+  port: parseInt(process.env.PORT, 10),
 }));
 
 export const appConfigSchema = {
@@ -12,4 +13,5 @@ export const appConfigSchema = {
     .required()
     .description('Application environment'),
   WALLET_PRIVATE_KEY: Joi.string().required().description('Wallet private key'),
+  PORT: Joi.number().default(3000).required().description('Application port'),
 };
