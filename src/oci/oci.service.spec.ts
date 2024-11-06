d6d2c65edfd966c15308f379f840c4ea71dd0d98
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-11-06 13:42:00 +0400

    feat: update oci-js-sdk

--- src/oci/oci.service.spec.ts
+++ src/oci/oci.service.spec.ts
@@ -1,13 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OciService } from './oci.service';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
+
+const mockPrivateKey = '0x';
+const mockConfigService = {
+  get: jest.fn((key: string) => {
+    if (key === 'app.walletPrivateKey') return mockPrivateKey;
+  }),
+};
 
 describe('OciService', () => {
   let service: OciService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OciService],
+      providers: [
+        OciService,
+        { provide: ConfigService, useValue: mockConfigService },
+      ],
       imports: [LoggerModule.forRoot()],
     }).compile();
 
